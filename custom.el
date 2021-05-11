47f7c42895e630ba355d49e2086bc49801ecf784
alphajuliet <andrew@alphajuliet.com>
2021-05-11 21:09:25 +1000

    First commit.

---
+++ custom.el
@@ -0,0 +1,9 @@
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:font #<font-spec nil nil Fira\ Code nil nil nil nil nil 13 nil nil nil nil>))))
+ '(variable-pitch ((t (:font #<font-spec nil nil Avenir\ Next nil nil nil nil nil 13 nil nil nil nil>)))))
