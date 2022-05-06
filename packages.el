acd2eaaee110ab92a4e2cec96d665528bf3fc56e
alphajuliet <andrew@alphajuliet.com>
2022-05-06 22:19:40 +1000

    Set darcula theme

--- packages.el
+++ packages.el
@@ -1,5 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
+;;;
+
+(package! darcula-theme)
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
